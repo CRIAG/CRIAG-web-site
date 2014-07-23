062cc849f9302f8e28c387d800608792533f061a
salmabenmoussa <benmoussasalma1@gmail.com>
2014-07-23 14:20:31 +0200

    email des messages

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'success',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
+		'message'=>'Merci de nous avoir contacté, nous vous répondrons le plutôt possible ! '
 	);
 
     $name = @trim(stripslashes($_POST['name'])); 
@@ -11,7 +11,7 @@
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = $email;
-    $email_to = 'email@email.com';//replace with your email
+    $email_to = 'elazizi@groupecriag.ma';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
